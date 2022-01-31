6a1c59ee640d64a0c69562e29a02ba67eeed8e00
Hugo <huseyin@gozutok.info>
2022-01-31 15:14:25 +0300

    bug

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -1,21 +1,21 @@
 import axios from "axios";
 import * as ApiUrl from "../constants/ApiUrl";
 
-export async function loginUser(user) {
+async function loginUser(user) {
   console.log(ApiUrl.userLogin);
   return await axios.post(ApiUrl.userLogin, user);
 }
 
-export async function registerUser(user) {
+async function registerUser(user) {
   return await axios.post(ApiUrl.userRegister, user);
 }
 
-export async function GetUser(user) {
+async function GetUser(user) {
   return await axios.get("http://localhost:8080/api/user/" + user);
 }
 
-export async function GetAllUsers() {
+async function GetAllUsers() {
   return await axios.get("http://localhost:8080/api/users");
 }
 
-export default { loginUser, registerUser, GetUser, GetAllUsers };
+export { loginUser, registerUser, GetUser, GetAllUsers };
